d27e8a5602eca38f375a1fd05aa7c66b1d4353c4
radityadimas <radityadimas43@gmail.com>
2019-09-24 00:43:43 +0700

    Update addOrder.php

--- process/addOrder.php
+++ process/addOrder.php
@@ -1,18 +1,16 @@
 <?php
 
 if(isset($_POST['addOrder'])){
-    include('../db.php');
- 
+    include('./db.php');
+
     $id = $_POST['id'];
     $nama_makanan = $_POST['nama_makanan'];
     $jumlah = $_POST['jumlah'];
     $pesan = $_POST['pesan'];
 
-    $order = mysqli_query($con,"INSERT INTO order SET nama_makanan='$nama_makanan',
-    jumlah='$jumlah,
-    pesan='$pesan'
-    WHERE id = '$id'")or die(mysqli_error($con));
-    if($order){
+    $input = mysqli_query($con,"INSERT INTO order(id,nama_makanan,jumlah,pesan)VALUES('$id','$nama_makanan','$jumlah','$pesan')")
+    or die(mysqli_error($con));
+    if($input){
         echo '<script>alert("successfully ordered!"); window.location = "../content/order.php"</script>';
     }else{
         echo '<script>alert("failed to update"); window.location = "../content/order.php"</script>';
