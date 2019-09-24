5fad52392533f70a9901905990e81597969a454f
Aileen <alishaileen05@gmail.com>
2019-09-24 20:07:35 +0700

    minor changes

--- layout/header.php
+++ layout/header.php
@@ -17,9 +17,9 @@ echo '<!DOCTYPE html>
   <body>
     <nav class="navbar is-link" role="navigation" aria-label="main navigation" style="background-color: #357ef4;">
       <div class="navbar-brand">
-        <a class="navbar-item" href="https://bulma.io">
-          <img src="https://bulma.io/images/bulma-logo.png" width="112" height="28">
-        </a>
+        // <a class="navbar-item" href="https://bulma.io">
+          <img src="../images/logo.png" width="112" height="28">
+        // </a>
 
         <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
           <span aria-hidden="true"></span>
