d81f8d87361fb87d08ee25cce4844f660149f866
aldiero141 <55279020+aldiero141@users.noreply.github.com>
2019-09-25 22:50:38 +0700

    Update prosesEditMenu.php

--- admin/proses/prosesEditMenu.php
+++ admin/proses/prosesEditMenu.php
@@ -5,7 +5,6 @@ if(isset($_POST['update'])){
     $id = $_POST['id'];
     $nama_makanan = $_POST['nama_makanan'];
     $deskripsi = $_POST['deskripsi'];
-    $profileImage = $_POST['profileImage'];
     $nama_promo = $_POST['nama_promo'];    
  
     // for the database
@@ -28,7 +27,7 @@ if(isset($_POST['update'])){
       $msg_class = "alert-danger";
     }
     if (empty($error)) {
-        if(move_uploaded_file($_FILES["profileImage"]["name"], $target_file)) {
+        if(move_uploaded_file($_FILES["profileImage"]["tmp_name"], $target_file)) {
             $input = mysqli_query($con,"UPDATE menu SET nama_makanan='$nama_makanan', deskripsi='$deskripsi', gambar='$gambarName', 
             nama_promo='$nama_promo' WHERE id='$id'")or die(mysqli_error($con));
 
